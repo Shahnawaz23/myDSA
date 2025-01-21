b0bc73cba8cf2e480141bf61cac194c4b0eab8f3
Shahnawaz <sa.akhtar23@gmail.com>
2025-01-22 01:25:24 +0530

    added AnyBaseToDecimal

---
+++ dsa/src/arrayAndFunctions/AnyBaseToDecimal.java
@@ -0,0 +1,25 @@
+package arrayAndFunctions;
+
+public class AnyBaseToDecimal {
+
+    public static void main(String[] args) {
+        int n = 1172;
+        int base = 8;
+
+        System.out.println(anyBaseToDecimal(n,base));
+    }
+
+    public static int anyBaseToDecimal(int n, int base) {
+        int result = 0;
+        int i = 0;
+        while (n > 0) {
+            int temp = n % 10;
+            int digit = (int) ( temp * Math.pow(base,i));
+            result += digit;
+            i++;
+            n /= 10;
+        }
+
+        return result;
+    }
+}
