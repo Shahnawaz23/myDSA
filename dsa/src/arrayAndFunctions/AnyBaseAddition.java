1c162ea08ec2dce9a7bfcea9a4f0b16d5b646a0a
Shahnawaz <sa.akhtar23@gmail.com>
2025-01-24 23:13:53 +0530

    changes in AnyBaseAddition

--- dsa/src/arrayAndFunctions/AnyBaseAddition.java
+++ dsa/src/arrayAndFunctions/AnyBaseAddition.java
@@ -2,8 +2,8 @@ package arrayAndFunctions;
 
 public class AnyBaseAddition {
     public static void main(String[] args) {
-        int n1 =  6542;
-        int n2 = 67342;
+        int n1 =  236;
+        int n2 = 754;
         int base = 8;
 
         System.out.println(baseAddition(n1, n2, base));
@@ -11,34 +11,53 @@ public class AnyBaseAddition {
 
     public static int baseAddition(int n1, int n2, int base) {
 
-        StringBuilder str = new StringBuilder();
+//        StringBuilder str = new StringBuilder();
 
         int carry = 0;
 
+//        while(n1 > 0 || n2 > 0 || carry > 0) {
+//            int digit1 = n1 % 10;
+//            int digit2 = n2 % 10;
+//
+//            int sum = digit1 + digit2 + carry;
+//
+//            if(sum >= base) {
+//                carry = 1;
+//                sum = sum % base;
+//            } else {
+//                carry = 0;
+//            }
+//
+//            str.append(sum);
+//
+//            n1 = n1/10;
+//            n2 = n2/10;
+//        }
+//
+//        str.reverse();
+
+//        String st = str.toString();
+//
+//        return Integer.parseInt(st);
+
+        int result = 0;
+        int p = 1;
+
         while(n1 > 0 || n2 > 0 || carry > 0) {
+
             int digit1 = n1 % 10;
             int digit2 = n2 % 10;
+            n1 = n1 / 10;
+            n2 = n2 / 10;
 
-            int sum = digit1 + digit2 + carry;
+            int digit = digit1 + digit2 + carry;
+            carry = digit / base;
+            digit = digit % base;
 
-            if(sum >= base) {
-                carry = 1;
-                sum = sum % base;
-            } else {
-                carry = 0;
-            }
-
-            str.append(sum);
-
-            n1 = n1/10;
-            n2 = n2/10;
+            result = result + digit * p;
+            p = p * 10;
         }
-
-        str.reverse();
-
-        String st = str.toString();
-
-        return Integer.parseInt(st);
+        return  result;
     }
 }
 
