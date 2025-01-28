3c412bc61f1f06c1a65228a0194adad380722ba5
Shahnawaz <sa.akhtar23@gmail.com>
2025-01-28 23:48:43 +0530

    added new ReverseArrayClass

---
+++ dsa/src/arrayAndFunctions/ReverseAnArray.java
@@ -0,0 +1,33 @@
+package arrayAndFunctions;
+
+import java.util.Scanner;
+
+public class ReverseAnArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for(int i = 0; i<n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        reverse(arr);
+    }
+
+    public static void reverse(int[] arr) {
+        int i = 0;
+        int j = arr.length - 1;
+        while(i<j) {
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            i++;
+            j--;
+        }
+
+        for (int value : arr) {
+            System.out.print(value + " ");
+        }
+    }
+}
