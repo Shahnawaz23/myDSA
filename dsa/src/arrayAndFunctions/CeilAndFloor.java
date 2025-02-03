ef73887fd698719e8f56f0443bba23b90944fabc
Shahnawaz <sa.akhtar23@gmail.com>
2025-02-04 00:08:50 +0530

    added CeilAndFlorr class

--- dsa/src/arrayAndFunctions/CeilAndFloor.java
+++ dsa/src/arrayAndFunctions/CeilAndFloor.java
@@ -19,6 +19,25 @@ public class CeilAndFloor {
     }
 
     public  static void findCielAndFloor(int[] arr, int data) {
-
+        int l = 0;
+        int h = arr.length - 1;
+        int ceil = 0;
+        int floor = 0;
+        while(l <= h) {
+            int mid = (l + h) / 2;
+            if(data < arr[mid]) {
+                h = mid - 1;
+                ceil = arr[mid];
+            } else  if(data > arr[mid]) {
+                l = mid + 1;
+                floor = arr[mid];
+            } else {
+                ceil = arr[mid];
+                floor = arr[mid];
+                break;
+            }
+        }
+        System.out.println(ceil);
+        System.out.println(floor);
     }
 }
