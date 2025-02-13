cc96cfb19057878ff087122ab3de3f180f7be269
Shahnawaz <sa.akhtar23@gmail.com>
2025-02-13 17:21:25 +0530

    added RemoveDuplicates class from leetcode

---
+++ dsa/src/leetcode/RemoveDuplicatesII.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+public class RemoveDuplicatesII {
+
+    public int removeDuplicates(int[] nums) {
+        int index = 2;
+        for(int i = 2; i<nums.length; i++) {
+            if(nums[i] != nums[index - 2]) {
+                nums[index] = nums[i];
+                index++;
+            }
+        }
+        return index;
+    }
+}
