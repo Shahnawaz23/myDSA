137ed5d3a43f6cd0dd18ff1a593c051204381c11
Shahnawaz <sa.akhtar23@gmail.com>
2025-02-25 18:05:45 +0530

    MinimumSizeSubarraySum leetCode

---
+++ dsa/src/leetcode/MinimumSizeSubarraySum.java
@@ -0,0 +1,24 @@
+package leetcode;
+
+public class g {
+    public int minSubArrayLen(int target, int[] nums) {
+        // Arrays.sort(nums);
+        int i = 0;
+        int j = 0;
+        int size = Integer.MAX_VALUE;
+        int sum = 0;
+
+        int n = nums.length;
+        while(j<n) {
+            sum += nums[j];
+            j++;
+            while(sum >= target) {
+                int currentSize = j - i;
+                size = Math.min(currentSize, size);
+                sum -= nums[i];
+                i++;
+            }
+        }
+        return (size == Integer.MAX_VALUE) ? 0 : size;
+    }
+}
